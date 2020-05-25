ad9b4e28eea57654fac158ea629558cfd1b34bb7
Vinit Bolla <vinitbolla10@gmail.com>
2020-05-25 15:50:54 +0530

    Added javascript to handle DOM

--- client/client.js
+++ client/client.js
@@ -1,1 +1,20 @@
-console.log('inside client.js')
+console.log('inside client.js');
+
+const form = document.querySelector('form');
+const loadingElement = document.querySelector('.loading');
+
+loadingElement.style.display = 'none';
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const content = formData.get('content');
+
+    const result = {
+        name, content
+    };
+    console.log(result);
+    form.style.display = 'none';
+    loadingElement.style.display = '';
+})
